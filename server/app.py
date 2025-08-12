5ef3c3146bdd7b07112034d691669782b7194004
ndurushima <ndu1994@gmail.com>
2025-08-11 22:03:53 -0500

    made some small changes here and there

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import os
-from flask import Flask, request, current_app, g, make_response
+from flask import Flask, request, current_app, g, jsonify
 
 contracts = [{"id": 1, "contract_information": "This contract is for John and building a shed"},{"id": 2, "contract_information": "This contract is for a deck for a buisiness"},{"id": 3, "contract_information": "This contract is to confirm ownership of this car"}]
 
@@ -21,23 +21,20 @@ def index():
         <h2>The name of this application is {appname}</h2>
         <h3>The path of this application on the user's device is {g.path}</h3>
     '''
-    status_code = 200
-    headers = {}
-    return make_response(response_body, status_code, headers)
+    return response_body, 200
 
-@app.route('/contract/<id>')
+@app.route('/contract/<int:id>')
 def get_contract(id):
-    if id in contracts:
-        return make_response(contracts, 200)
-    else:
-        return make_response({"error": "Contract not found"}, 404)
+    contract = next((c for c in contracts if c["id"] == id), None)
+    if contract:
+        return contract["contract_information"], 200
+    return jsonify({"error": "Contract not found"}), 404
 
 @app.route('/customer/<customer_name>')
 def get_customer(customer_name):
     if customer_name in customers:
-        return make_response({"customer": customer_name}, 200)
-    else:
-        return make_response({"error": "Customer not found"}, 404)
+        return "", 204
+    return "", 404
     
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
